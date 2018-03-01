7079e79b6cb7cb7b22778d8bfcfc6ff03e5966e7
Wangc <857198640@qq.com>
2018-03-01 18:38:22 +0800

    加个分号

--- js/we.account.management.js
+++ js/we.account.management.js
@@ -3,16 +3,16 @@ function we_register_person(email, pwd, pwd_rpt){
     // loginResult: 0 if OK, <0 for different error codes
     // returnComment: some comments for display
     // otherData: not defined yet
-    var loginResult = -1
-    var returnComment = ""
-    var otherData = ""
+    var loginResult = -1;
+    var returnComment = "";
+    var otherData = "";
     if(typeof(Storage) !== "undefined") {
         if (pwd == pwd_rpt) {
             returnComment = "user " + email + " registered!"
-            localStorage.email = email
-            localStorage.pwd = pwd
+            localStorage.email = email;
+            localStorage.pwd = pwd;
         } else {
-            returnCommentL = "passwords not match"
+            returnCommentL = "passwords not match";
         }
     } else {
         returnComment = "Sorry, your browser does not support web storage.";
@@ -21,23 +21,23 @@ function we_register_person(email, pwd, pwd_rpt){
 }
 
 function we_login_person(email, pwd){
-    var loginResult = -1
-    var returnComment = ""
-    var otherData = ""
+    var loginResult = -1;
+    var returnComment = "";
+    var otherData = "";
     if(typeof(Storage) !== "undefined") {
         if (localStorage.email == email){
-            returnComment = "found user name " + localStorage.email
+            returnComment = "found user name " + localStorage.email;
             if (localStorage.pwd == pwd) {
                 returnComment += " login ok!";
             }
             else{
-                returnComment += " password not match"
+                returnComment += " password not match";
             }
         } else {
-            returnComment = "not found user name " + email
+            returnComment = "not found user name " + email;
         }
     } else {
         returnComment = "Sorry, your browser does not support web storage.";
     }
-    return [loginResult,returnComment,otherData]
+    return [loginResult,returnComment,otherData];
 }
